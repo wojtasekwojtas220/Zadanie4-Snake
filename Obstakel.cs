80930da8df55858d4e59fb52e8ba1084aecae62b
wojtasekwojtas220 <g.wojtek02@gmail.com>
2025-05-27 19:57:46 +0200

    Add files via upload

---
+++ Obstakel.cs
@@ -0,0 +1,13 @@
+﻿public class Obstakel
+
+{
+
+    public int Xpos { get; set; }
+
+    // ?
+
+    public ConsoleColor schermKleur { get; set; }
+
+    public string karacter { get; set; }
+
+}
---
+++ Pixel.cs
@@ -0,0 +1,13 @@
+﻿public class Pixel
+
+{
+
+    public int xPos { get; set; }
+
+    public int yPos { get; set; }
+
+    public ConsoleColor schermKleur { get; set; }
+
+    public string karacter { get; set; }
+
+}
